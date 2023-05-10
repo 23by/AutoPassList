0a3d73353b760e91b4879ac2855d918cfb111a72
23by <100278764+23by@users.noreply.github.com>
2023-05-10 20:45:37 +0400

    Add files via upload

---
+++ AutoPassWord.py
@@ -0,0 +1,22 @@
+
+import random
+import string
+
+def generate_wordlist(length):
+    wordlist = []
+    characters = string.ascii_letters + string.digits + "!@#$"
+    for _ in range(10000000):
+        password = ''.join(random.choice(characters) for _ in range(length))
+        wordlist.append(password)
+    return wordlist
+
+def save_wordlist(wordlist, filename):
+    with open(filename, 'w') as file:
+        file.write('\n'.join(wordlist))
+    print(f"The list has been saved to file: {filename}")
+
+length = 11  
+filename = 'wordlist.txt'
+
+wordlist = generate_wordlist(length)
+save_wordlist(wordlist, filename)
---
+++ README.txt
@@ -0,0 +1,8 @@
+wordlist typing tool python script | ten million pass in 34s
+
+tool made by: bye - Discord: bye#6193
+
+Github: https://github.com/23by
+
+Thank you for using the tool
+<3
